6cbf3c5131c2c1e1e795ef5b1b689b9c8810e57e
Milton Jesus <milton.lima@outlook.com>
2022-05-27 23:06:15 -0300

    Unpublished function

--- math.go
+++ math.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(Soma(1, 2))
+	fmt.Println(soma(1, 2))
 
 }
 
-func Soma(a int, b int) int {
+func soma(a int, b int) int {
 	return a + b
 }
